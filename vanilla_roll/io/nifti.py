bbedc2a5d0737d8839c9b74b1a10b8c96509fdb7
Masahiro Wada <argon.argon.argon@gmail.com>
2022-10-25 06:05:26 +0000

    fix: fix nifti dependency

--- vanilla_roll/io/nifti.py
+++ vanilla_roll/io/nifti.py
@@ -36,41 +36,36 @@ class NIFTIIOParams:
     pass
 
 
-def _get_origin(nii: SpatialImage) -> Vector:
-    translates: npt.NDArray[np.float64] = nii.affine[:3, 3]
-    return Vector(
-        i=translates[2],
-        j=translates[1],
-        k=translates[0],
-    )
-
-
-def _get_orientation(nii: SpatialImage) -> Orientation:
-    orientation: npt.NDArray[np.float64] = nii.affine[:3, :3]
-    return Orientation(
-        i=Vector.of_array(orientation[2]),
-        j=Vector.of_array(orientation[1]),
-        k=Vector.of_array(orientation[0]),
-    )
-
-
-def _get_anatomy_orientation(nii: SpatialImage) -> AnatomyOrientation:
-    axcodes: tuple[str] = aff2axcodes(nii.affine)
-    return parse_anatomy_orientation("".join(axcodes[::-1]))
-
-
-def _get_data(nii: SpatialImage) -> xp.Array:
-    return xp.from_dlpack(nii.get_data())
-
-
-def _load_data(path: str | Path) -> SpatialImage:
-    image: SpatialImage = nib.load(path)
-    return cast(SpatialImage, image)
-
-
 def read_nifti(path: str | Path, params: NIFTIIOParams = NIFTIIOParams()) -> Volume:
     if not HAS_NIBABEL:
         raise RuntimeError("nibabel is not installed")
+
+    def _get_origin(nii: SpatialImage) -> Vector:
+        translates: npt.NDArray[np.float64] = nii.affine[:3, 3]
+        return Vector(
+            i=translates[2],
+            j=translates[1],
+            k=translates[0],
+        )
+
+    def _get_orientation(nii: SpatialImage) -> Orientation:
+        orientation: npt.NDArray[np.float64] = nii.affine[:3, :3]
+        return Orientation(
+            i=Vector.of_array(orientation[2]),
+            j=Vector.of_array(orientation[1]),
+            k=Vector.of_array(orientation[0]),
+        )
+
+    def _get_anatomy_orientation(nii: SpatialImage) -> AnatomyOrientation:
+        axcodes: tuple[str] = aff2axcodes(nii.affine)
+        return parse_anatomy_orientation("".join(axcodes[::-1]))
+
+    def _get_data(nii: SpatialImage) -> xp.Array:
+        return xp.asarray(nii.get_data())
+
+    def _load_data(path: str | Path) -> SpatialImage:
+        image: SpatialImage = nib.load(path)
+        return cast(SpatialImage, image)
 
     nii = _load_data(path)
 
